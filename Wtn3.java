496e3e359d79705013e5b26e2727ee0d0f262a88
kancharladheeraj <58482086+kancharladheeraj@users.noreply.github.com>
2019-12-04 14:07:35 +0530

    Add files via upload
    MileStone1- Language Basics

---
+++ Wtn3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Wtn3
+{
+	public static void main(String args[])
+	{	
+		       
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("The sum of "+a+"and "+b+" is "+(a+b)); 
+	}	
+}
